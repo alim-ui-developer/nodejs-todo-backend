a86e509f96404eea57623257e57b9366beaf8b3b
alim <dahye_a@naver.com>
2024-05-24 22:28:49 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+/*
+ * 할일 앱
+
+restful API 
+주소 + http명령어
+
+1. 할 일을 추가할 수 있다. C  /tasks post
+2. 할 일 리스트를 볼 수 있다. R  /tasks get
+3. 할 일에 대해서 끝남, 안끝남 표시를 할 수 있다. U  /tasks/:id put
+4. 할 일을 삭제할 수 있다. D  /tasks/:id
+
+백엔드 준비
+1. 기본 셋팅: npm, express, mongoose, app 리스너
+2. 라우터를 정의한다
+3. 데이터 베이스 스키마 정의
+4. 기능 정의: CRUD
+5. 테스트: 포스트맨!!!!!!!!!!!!!!
+
+프론트앤드 준비
+1. 깃 클론
+2. 기능 만들기: CRUD
+3. 테스트:
+*/
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api', indexRouter);
+
+const mogoURI = `monggodb://localhost:27017/todo-demo`;
+
+// 몽고디비의 새로운 주소형태가 있다(옛날형식뿐만 아니라 요즘형식도 잘 도와달라는 의미)
+mongoose
+.connect(mogoURI, {useNewUrlParser: true })
+.then(() => {
+  console.log('mongoose connected!')
+})
+.catch((err) => {
+  console.log("DB connection fail", err)
+});
+
+
+// app은 port number 5000번을 계속 주시할거다
+app.listen(5000, () => {
+  console.log("server on 5000");
+});
---
+++ controller/task.controller.js
@@ -0,0 +1,53 @@
+const taskController = {}
+
+taskController.createTask = async (req, res) => {
+  try{
+    const { task, isComplete } = req.body;
+    const newTask = new Task({task, isComplete});
+    await newTask.save();
+    res.status(200).json({ status: '200', data: newTask});
+  } catch (err) {
+    res.status(400).json({ status: 'fail', error: err});
+  }
+}
+
+taskController.getTask = async (req, res) => {
+  try{
+    const taskList = await Task.find({}); // 조건 없이 모든 리스트 가져오기
+    res.status(200).json({ status: "ok", data: taskList});
+  } catch (err) {
+    res.status(400).json({ status: 'fail', error: err});
+  }
+}
+
+taskController.updateTask = async (req, res) => {
+  try{
+    const { id } = req.params;
+    const { task, isComplete } = req.body;
+
+    const post = await Post.findById(id);
+  
+    // 수정
+    post.title = title;
+    post.description = description;
+    
+    await post.save();
+
+    res.status(200).json({ status: "success", data: Post});
+  } catch (err) {
+    res.status(400).json({ status: 'fail', error: err});
+  }
+}
+
+
+taskController.deleteTask = async (req, res) => {
+  try{
+    const deleteItem = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ status: "success", data: deleteItem });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', error: err});
+  }
+}
+
+
+module.exports = taskController;
